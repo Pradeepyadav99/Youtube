72ddfbc54cb8165c5d6894214c1bd920972b3195
Pradeepyadav99 <mail4upky@gmail.com>
2024-07-29 00:01:06 +0530

    login changes

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -56,11 +56,11 @@ userSchema.pre("save", async function(next){
     next() 
 });
 
-userSchema.method.isPasswordCorrect = async function(password){
+userSchema.methods.isPasswordCorrect = async function(password){
     return await bcrypt.compare(password, this.password);
 }
 
-userSchema.method.generateAccessToken = function(){
+userSchema.methods.generateAccessToken = function(){
 return jwt.sign(
         {
             _id: this.id,
@@ -75,7 +75,7 @@ return jwt.sign(
     )
 }
 
-userSchema.method.generateRefreshToken = function(){
+userSchema.methods.generateRefreshToken = function(){
     return jwt.sign(
             {
                 _id: this.id
